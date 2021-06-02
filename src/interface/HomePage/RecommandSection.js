7e862e7875b39da0afae138b8512ee7dac7bedcc
glenn <glenn@simpleinfo.cc>
2021-06-03 01:08:57 +0800

    Remove google api key.

--- src/interface/HomePage/RecommandSection.js
+++ src/interface/HomePage/RecommandSection.js
@@ -3,7 +3,7 @@ import React, { useEffect, useRef } from 'react';
 const InputerSection = ({ onGetUrl }) => {
 
   useEffect(() => {
-    // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=bbc+learning+english&maxResults=10&order=viewCount&key=AIzaSyBpHzkztUGk60xVK7quXoj7R6sijBJ5aMY`, {
+    // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=bbc+learning+english&maxResults=10&order=viewCount&key=`, {
     //   method: 'GET',
     // })
     // .then(res => {
