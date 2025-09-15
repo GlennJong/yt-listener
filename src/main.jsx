c0c97b7ef949c69945e5beac8c987f9faa29fd5f
GlennJong <zaku0225@gmail.com>
2025-09-15 17:32:33 +0000

    Update development tool to vite.js, and ready to reconnect with new transcript api.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import wordLibrary from './store/wordLibrary';
 import videoData from './store/videoData';
 import configData from './store/configData';
@@ -28,7 +27,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
