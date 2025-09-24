c04d0fd3c3ccdd71206e309dc24d8f34e1e4dcc3
GlennJong <zaku0225@gmail.com>
2025-09-24 15:47:37 +0000

    Define demo mode on config page

--- src/interface/ConfigPage/index.jsx
+++ src/interface/ConfigPage/index.jsx
@@ -5,8 +5,7 @@ import configData from '../../store/configData';
 import InputItem from '../../components/InputItem';
 import { color } from '../../constant/color';
 
-// const isDemo = import.meta.env.VITE_IS_DEMO;
-const isDemo = true;
+const isDemo = import.meta.env.VITE_IS_DEMO;
 
 const ConfigPage = ({ currentPage }) => {
   const {
